a8e1c046e7545cbba97a1208e09ddec4010bf073
Tosh Meston <tosh.meston@gmail.com>
2014-05-11 00:17:08 -0700

    Removed test file.

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var fs = require('fs');
 // Set up static folders.
 //
 app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
-app.use('/data', express.static(__dirname + '/data'));
 app.use('/images', express.static(__dirname + '/images'));
 app.use('/scripts', express.static(__dirname + '/scripts'));
 app.use('/styles', express.static(__dirname + '/styles'));
@@ -30,10 +29,6 @@ app.get('/finder', function(req, res) {
     app.locals.css = 'finder.css';
     res.render('finder.ejs');
 });
-
-app.get('/test', function(req, res) {
-    res.render('test.ejs');
-}); 
 
 app.listen(3000);
 console.log('app is listening at localhost:3000');
