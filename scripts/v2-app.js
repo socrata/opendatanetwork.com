73f2033aba3f2fdd7dfe6f3613ebe0b8d0862f58
Tosh Meston <tosh.meston@gmail.com>
2015-04-20 18:28:45 -0700

    Added Communities menu. Added code to scroll to top when Search icon is clicked.

---
+++ scripts/v2-app.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+    // Communities menu
+    //
+    $('#menu-item-communities').mouseenter(function() {
+
+        $('#menu-communities').show(); 
+        $('#menu-item-communities').addClass('selected');
+    });
+
+    $('#menu-item-communities, #menu-communities').mouseleave(function() {
+
+        $('#menu-communities').hide(); 
+        $('#menu-item-communities').removeClass('selected');
+    });
+
+    // Search icon in header
+    //
+    $('.fa-search').click(function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 
+        400);
+    });
+});
