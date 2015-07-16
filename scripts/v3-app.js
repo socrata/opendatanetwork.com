c16f3763bee729d22bccb2bc7e43f9bdedc424d0
Tosh Meston <tosh.meston@gmail.com>
2015-07-15 18:31:34 -0700

    Added the search options menu and hooked up its functionality.

--- scripts/v3-app.js
+++ scripts/v3-app.js
@@ -25,4 +25,106 @@ $(document).ready(function() {
         else
             $('#form').submit();
     });
+
+    // Search menu controller
+    //
+    new SearchMenuController();
 });
+
+// Search menu controller
+function SearchMenuController() {
+
+    this.categories = this.getParameterByName('categories').split2(',');
+    var self = this;
+
+    // Attach handler to the search menu button
+    //
+    $('.search-options-link').click(function() { $('.search-options-menu').toggle(); });
+
+    // Attach handlers to the all categories link
+    //
+    $('.search-options-menu-all').click(function() { self.clearCategories(); });
+
+    // Attach handlers to the individual category links
+    //
+    $('.search-options-menu li a').click(function() { self.toggleCategory($(this)); });
+
+    this.updateMenuState();
+}
+
+// Public methods
+//
+SearchMenuController.prototype.clearCategories = function() {
+
+    this.categories = [];
+
+    $('#categories').val('');
+    console.log($('#categories').val());
+
+    this.updateMenuState();
+};
+
+SearchMenuController.prototype.getParameterByName = function(name) {
+
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
+    var results = regex.exec(location.search);
+
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+};
+
+SearchMenuController.prototype.toggleCategory = function(o) {
+
+    var category = o.text().toLowerCase().trim();
+    var i = this.categories.indexOf(category);
+
+    if (i > -1)
+        this.categories.splice(i, 1); // remove at index i
+    else
+        this.categories.push(category);
+
+    $('#categories').val(this.categories.join(','));
+    console.log($('#categories').val());
+
+    this.updateMenuState();
+};
+
+SearchMenuController.prototype.updateMenuState = function() {
+
+    var self = this;
+
+    // Update the all categories checkbox
+    //
+    if (this.categories.length == 0)
+        $('.search-options-menu-all .fa-check').show();
+    else
+        $('.search-options-menu-all .fa-check').hide();
+
+    // Update the individual category checkboxes
+    //
+    $('.search-options-menu li a').each(function() {
+
+        if (self.categories.includes($(this).text().toLowerCase().trim()))
+            $(this).children('.fa-check').show();
+        else
+            $(this).children('.fa-check').hide();
+    });
+};
+
+// Extensions
+//
+String.prototype.split2 = function(s) {
+
+    var rg = this.split(s);
+
+    if ((rg.length == 1) && (rg[0] == ''))
+        return [];
+
+    return rg;
+};
+
+Array.prototype.includes = function(s) {
+
+    return this.indexOf(s) > -1;
+};
