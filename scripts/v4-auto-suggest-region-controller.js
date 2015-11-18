0b344a046adde16f35ad93e8dd86ad9203b6b121
Lane Aasen <laneaasen@gmail.com>
2015-11-17 16:13:04 -0800

    starts work on integrating better autocomplete

--- scripts/v4-auto-suggest-region-controller.js
+++ scripts/v4-auto-suggest-region-controller.js
@@ -1,3 +1,40 @@
+
+class AutocompleteResult {
+    constructor(type, title) {
+        this.type = type;
+        this.title = title;
+    }
+
+    show(selection) {
+        selection
+            .append('p')
+            .text(this.title)
+    }
+}
+
+
+class Complete {
+    constructor(type, queryBuilder) {
+        this.type = type;
+        this.queryBuilder = queryBuilder;
+    }
+
+    get(query) {
+        if (query == '') {
+            return [];
+        } else {
+            return $.getJSON(this.queryBuilder(query))
+                .then(response => {
+                    return response.options.map(option => {
+                        return new AutocompleteResult(this.type, option.text);
+                    });
+                });
+        }
+    }
+}
+
+
+
 class AutoSuggestRegionController {
 
     constructor(inputTextSelector, resultListSelector, onClickRegion) {
@@ -9,6 +46,15 @@ class AutoSuggestRegionController {
         this.onClickRegion = onClickRegion;
 
         var autoSuggestDelay = 150;
+
+        function autocompleteURL(domain, fxf, column) {
+            return query => `https://${domain}/views/${fxf}/columns/${column}/suggest/${query}?size=5`;
+        }
+
+        const domain = 'odn.data.socrata.com';
+
+        const datasetURL = autocompleteURL(domain, 'fpum-bjbr', 'name');
+        this.datasetComplete = new Complete('Dataset', datasetURL);
 
         // Keyboard event
         //
@@ -31,18 +77,18 @@ class AutoSuggestRegionController {
                 return;
             }
             else if (e.keyCode == 13) { // enter
-    
+
                 if ((this.selectedIndex == -1) || (this.selectedIndex >= this.options.length))
                     return;
 
                 e.preventDefault();
 
-                if (this.onClickRegion) 
+                if (this.onClickRegion)
                     this.onClickRegion(this.options[this.selectedIndex].text);
 
                 return;
             }
-            else if (e.keyCode == 27) { // esc 
+            else if (e.keyCode == 27) { // esc
 
                 $(resultListSelector).slideUp(100);
                 return;
@@ -51,7 +97,7 @@ class AutoSuggestRegionController {
             // Clear the timer
             //
             clearTimeout(this.timer);
-    
+
             // Get the search value
             //
             var searchTerm = $(inputTextSelector).val().trim();
@@ -71,46 +117,51 @@ class AutoSuggestRegionController {
     }
 
     autoSuggest(searchTerm, resultListSelector) {
+        console.log(searchTerm);
+
+        this.datasetComplete.get(searchTerm).then(results => console.log(results));
+
 
         var controller = new ApiController();
 
         controller.getAutoCompleteNameSuggestions(searchTerm)
             .then(data => {
-    
+
+
                 this.options = data.options;
                 this.selectedIndex = -1;
-    
+
                 var regionList = $(resultListSelector);
-    
+
                 if (this.options.length == 0) {
-    
+
                     regionList.slideUp(100);
                     return;
                 }
-    
+
                 // Build the list items
                 //
                 var items = this.options.map(function(item) { return '<li>' + item.text + '</li>'; });
                 regionList.html(items.join(''));
-    
+
                 // Click event
                 //
                 var self = this;
 
                 $(resultListSelector + ' li').click(function() {
-    
-                    if (self.onClickRegion) 
+
+                    if (self.onClickRegion)
                         self.onClickRegion(self.options[$(this).index()].text);
                 });
-    
+
                 // Mouse event
-                //   
+                //
                 $(resultListSelector + ' li').mouseenter(function() {
-    
+
                     self.selectedIndex = $(this).index();
                     self.updateListSelection();
                 });
-    
+
                 // Slide the list down
                 //
                 regionList.slideDown(100);
