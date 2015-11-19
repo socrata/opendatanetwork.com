57481ad662c3144e9a9e2a5cbfd865444e25bdea
Lane Aasen <laneaasen@gmail.com>
2015-11-18 17:01:29 -0800

    integrates autocomplete by source

--- scripts/v4-search-page-controller.js
+++ scripts/v4-search-page-controller.js
@@ -1,6 +1,9 @@
 
-function mainAutocomplete(inputSelector, resultSelector) {
-    function autocompleteURL(domain, fxf, column) {
+const selectByTopic = (() => {
+    const domain = 'odn.data.socrata.com';
+    const fxf = 'pfgp-ifph';
+
+    function urlFor(column) {
         return query => `https://${domain}/views/${fxf}/columns/${column}/suggest/${query}?size=5`;
     }
 
@@ -8,35 +11,29 @@ function mainAutocomplete(inputSelector, resultSelector) {
         window.location.href = path;
     }
 
-    const domain = 'odn.data.socrata.com';
-    const inputSelection = d3.select(inputSelector);
-    const resultSelection = d3.select(resultSelector);
+    function complete(column) {
+        return (inputSelector, resultSelector) => {
+            const inputSelection = d3.select('.add-region-input');
+            const resultSelection = d3.select('.add-region-results');
 
-    const datasetURL = autocompleteURL(domain, 'fpum-bjbr', 'name');
-    const datasetSelect = dataset => navigate(`/search?q=${dataset}`);
-    const datasetResults = new Results('Datasets', resultSelection, datasetSelect);
-    const datasetComplete = new Complete(datasetURL, datasetResults);
+            const url = urlFor(column);
+            const select = region => navigate(`/search?q=${region}`);
+            const results = new Results('Regions with Data', resultSelection, select);
+            const complete = new Complete(url, results);
 
-    const regionURL = autocompleteURL(domain, '7g2b-8brv', 'autocomplete_name');
-    const regionSelect = region => navigate(`/${region.replace(/ /g, '_')}`);
-    const regionResults = new Results('Regions', resultSelection, regionSelect);
-    const regionComplete = new Complete(regionURL, regionResults);
+            new AutoSuggestRegionController(inputSelection, resultSelection, [complete]);
+        }
+    }
 
-    const publisherURL = autocompleteURL(domain, '8ae5-ghum', 'domain');
-    const publisherSelect = publisher => navigate(`/search?domains=${publisher}`);
-    const publisherResults = new Results('Publishers', resultSelection, publisherSelect);
-    const publisherComplete = new Complete(publisherURL, publisherResults);
-
-    const categoryURL = autocompleteURL(domain, '864v-r7tf', 'category');
-    const categorySelect = category => navigate(`/search?categories=${category}`);
-    const categoryResults = new Results('Categories', resultSelection, categorySelect);
-    const categoryComplete = new Complete(categoryURL, categoryResults);
-
-    const completers = [datasetComplete, regionComplete,
-                        publisherComplete, categoryComplete];
-
-    return new AutoSuggestRegionController(inputSelection, resultSelection, completers);
-}
+    const selects = new Map();
+    selects.set('population', complete('population'));
+    selects.set('earnings', complete('earnings'));
+    selects.set('education', complete('education'));
+    selects.set('occupations', complete('occupations'));
+    selects.set('cost_of_living', complete('rpp'));
+    selects.set('gdp', complete('gdp'));
+    return selects;
+})();
 
 
 class SearchPageController {
@@ -159,7 +156,15 @@ class SearchPageController {
 
         // Add location
         //
-        mainAutocomplete('.add-region input[type="text"]', '.add-region ul');
+        const inputSelector = '.add-region-input';
+        const resultSelector = '.add-region-results';
+        const defaultSuggest = selectByTopic.get('population');
+        const vector = this.params.vector;
+        if (selectByTopic.has(vector)) {
+            selectByTopic.get(vector)(inputSelector, resultSelector);
+        } else {
+            defaultSuggest(inputSelector, resultSelector);
+        }
 
         $('.add-region .fa-plus').click(function() {
 
--- scripts/v4-search.js
+++ scripts/v4-search.js
@@ -1,3 +1,43 @@
+
+function mainAutocomplete(inputSelector, resultSelector) {
+    function autocompleteURL(domain, fxf, column) {
+        return query => `https://${domain}/views/${fxf}/columns/${column}/suggest/${query}?size=5`;
+    }
+
+    function navigate(path) {
+        window.location.href = path;
+    }
+
+    const domain = 'odn.data.socrata.com';
+    const inputSelection = d3.select(inputSelector);
+    const resultSelection = d3.select(resultSelector);
+
+    const datasetURL = autocompleteURL(domain, 'fpum-bjbr', 'name');
+    const datasetSelect = dataset => navigate(`/search?q=${dataset}`);
+    const datasetResults = new Results('Datasets', resultSelection, datasetSelect);
+    const datasetComplete = new Complete(datasetURL, datasetResults);
+
+    const regionURL = autocompleteURL(domain, '7g2b-8brv', 'autocomplete_name');
+    const regionSelect = region => navigate(`/${region.replace(/ /g, '_')}`);
+    const regionResults = new Results('Regions', resultSelection, regionSelect);
+    const regionComplete = new Complete(regionURL, regionResults);
+
+    const publisherURL = autocompleteURL(domain, '8ae5-ghum', 'domain');
+    const publisherSelect = publisher => navigate(`/search?domains=${publisher}`);
+    const publisherResults = new Results('Publishers', resultSelection, publisherSelect);
+    const publisherComplete = new Complete(publisherURL, publisherResults);
+
+    const categoryURL = autocompleteURL(domain, '864v-r7tf', 'category');
+    const categorySelect = category => navigate(`/search?categories=${category}`);
+    const categoryResults = new Results('Categories', resultSelection, categorySelect);
+    const categoryComplete = new Complete(categoryURL, categoryResults);
+
+    const completers = [datasetComplete, regionComplete,
+                        publisherComplete, categoryComplete];
+
+    return new AutoSuggestRegionController(inputSelection, resultSelection, completers);
+}
+
 $(document).ready(function() {
 
     // Search page controller
@@ -6,11 +46,7 @@ $(document).ready(function() {
 
     // Main search box
     //
-    new AutoSuggestRegionController('#q', '.region-list', function(region) {
-
-        searchPageController.setAutoSuggestedRegion(region, true);
-        searchPageController.navigate();
-    });
+    mainAutocomplete('#q', '.region-list');
 
     // Chart column
     //
@@ -37,11 +73,11 @@ $(document).ready(function() {
             case 'cost_of_living':
                 searchPageController.drawCostOfLivingData();
                 break;
-                
+
             case 'gdp':
                 searchPageController.drawGdpData();
                 break;
-                
+
             default:
                 searchPageController.drawPopulationData();
                 break;
