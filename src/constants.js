236bda3a98f8559866819853aa0fb3ed4a39e629
Lane Aasen <laneaasen@gmail.com>
2015-11-24 13:59:41 -0800

    moves limit down to 5000 for performance

--- src/constants.js
+++ src/constants.js
@@ -3,6 +3,6 @@ const Constants = {
     ROSTER_URL: 'https://federal.demo.socrata.com/resource/7g2b-8brv.json',
 
     // maximum number of results from Socrata
-    LIMIT: 50000
+    LIMIT: 5000
 };
 
