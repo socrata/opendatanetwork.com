4f033b632a4c5fe5dd0f9b5cde3a4f2cee12168d
Lane Aasen <laneaasen@gmail.com>
2015-11-29 18:26:14 -0800

    updates sourcecomplete

--- src/data/autosuggest-sources.js
+++ src/data/autosuggest-sources.js
@@ -1,37 +1,24 @@
 
-// Autocomplete to regions with data for the given source.
-const sourceComplete = (() => {
-    const domain = 'odn.data.socrata.com';
-    const fxf = 'pfgp-ifph';
+const domain = 'odn.data.socrata.com';
 
-    const nameToColumn = new Map();
-    nameToColumn.set('population', 'population');
-    nameToColumn.set('earnings', 'earnings');
-    nameToColumn.set('education', 'education');
-    nameToColumn.set('occupations', 'occupations');
-    nameToColumn.set('cost_of_living', 'rpp');
-    nameToColumn.set('gdp', 'gdp');
+const _nameToColumn = new Map();
+_nameToColumn.set('population', 'population');
+_nameToColumn.set('earnings', 'earnings');
+_nameToColumn.set('education', 'education');
+_nameToColumn.set('occupations', 'occupations');
+_nameToColumn.set('cost_of_living', 'rpp');
+_nameToColumn.set('gdp', 'gdp');
+const defaultColumn = 'population';
 
-    const defaultColumn = 'population';
+const _withDataSource = {
+    name: 'Regions with Data',
+    domain: domain,
+    fxf: 'pfgp-ifph'
+};
 
-    function urlFor(column) {
-        return query => `https://${domain}/views/${fxf}/columns/${column}/suggest/${query}?size=5`;
-    }
+function regionsWithData(name, select) {
+    const column = _nameToColumn.has(name) ? _nameToColumn.get(name) : defaultColumn;
 
-    function navigate(path) {
-        window.location.href = path;
-    }
-
-    return (inputSelector, resultSelector, name, select) => {
-        const inputSelection = d3.select(inputSelector);
-        const resultSelection = d3.select(resultSelector);
-
-        const column = nameToColumn.has(name) ? nameToColumn.get(name) : defaultColumn;
-        const url = urlFor(column);
-        const results = new Results('Regions with Data', resultSelection, select);
-        const complete = new Complete(url, results);
-
-        return new AutoSuggestRegionController(inputSelection, resultSelection, [complete]);
-    };
-})();
+    return [_.extend({}, _withDataSource, {select, column})];
+}
 
