925c630cc2c28464d13c564d6b5b988fead9c98d
Tosh Meston <tosh.meston@gmail.com>
2015-12-14 17:26:37 -0800

    Fixes i - source mouseover text #111

--- src/search.js
+++ src/search.js
@@ -14,7 +14,17 @@ $(document).ready(function() {
     $('.fa-close').click(function() {
         $('.current-category').fadeOut();
     });
-    
+
+    // Tooltip
+    //
+    $('.info-icon').mouseenter(function() {
+        $('.info-tooltip').fadeIn();
+    });
+
+    $('.info-icon').mouseleave(function() {
+        $('.info-tooltip').fadeOut();
+    });
+
     // Chart column
     //
     if (_params.regions.length > 0) {
