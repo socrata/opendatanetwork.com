f0082880b07fb1d3fc9d6532d0a99d32834b8b6c
Tosh Meston <tosh.meston@gmail.com>
2016-01-07 16:28:20 -0800

    Fixed bug in getSearchDatasetsUrl where empty param arrays caused a trailing & on the URL.

--- controllers/api-controller.js
+++ controllers/api-controller.js
@@ -106,6 +106,7 @@ ApiController.prototype.getSearchDatasetsUrl = function(requestParams, completio
     const synonyms = _.unique(_.flatten([querySynonyms, vectorSynonyms]));
 
     const regionNames = requestParams.regions.map(region => {
+
         const name = region.name;
         const type = region.type;
 
@@ -136,7 +137,7 @@ ApiController.prototype.getSearchDatasetsUrl = function(requestParams, completio
         tags : tags,
     });
 
-    const params = _.omit(allParams, value => value === '' || value === []);
+    const params = _.omit(allParams, value => value === '' || _.isEmpty(value));
     const url = `${Constants.CATALOG_URL}?${querystring.stringify(params)}`;
 
     completionHandler(url);
