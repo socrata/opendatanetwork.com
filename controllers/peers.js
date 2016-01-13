c549caafd2e6af7cf8cf8b273156f1034efb5455
Lane Aasen <laneaasen@gmail.com>
2016-01-13 14:01:27 -0800

    adds caching to peers

--- controllers/peers.js
+++ controllers/peers.js
@@ -2,33 +2,58 @@
 
 const request = require('request');
 const _ = require('lodash');
+const NodeCache = require('node-cache');
+
+const peerCache = new NodeCache({stdTTL: 0});
+
 
 class Peers {
-    static fromParams(params) {
-        const regions = params.regions;
+    static get(uid) {
+        const n = 10;
 
         return new Promise((resolve, reject) => {
+            const key = `${uid}-${n}`;
+
+            peerCache.get(key, (error, value) => {
+                if (value === undefined) {
+                    const url = `https://odn-peers.herokuapp.com/peers/${uid}?n=${n}`;
+                    request.get(url, (error, response, body) => {
+                        if (error || response.statusCode != 200) {
+                            console.log(`error ${response.statusCode} for ${url}`);
+                            resolve([]);
+                        } else {
+                            const peers = JSON.parse(body).peers;
+                            peerCache.set(key, peers);
+                            resolve(JSON.parse(body).peers);
+                        }
+                    });
+                } else {
+                    resolve(value);
+                }
+            });
+        });
+    }
+
+    static fromParams(params) {
+        return new Promise((resolve, reject) => {
+            const regions = params.regions;
             if (regions.length === 0) {
                 resolve([]);
             } else {
                 const uid = regions[0].id;
-                const url = `https://odn-peers.herokuapp.com/peers/${uid}?n=10`;
-                request.get(url, (error, response, body) => {
-                    if (error || response.statusCode != 200) {
-                        console.log(`error ${response.statusCode} for ${url}`);
-                        resolve([]);
-                    } else {
-                        const uids = regions.map(region => region.id);
-                        const vector = params.vector;
-                        resolve(JSON.parse(body).peers.filter(peer => {
-                            return uids.indexOf(peer.id) < 0;
-                        }).map(peer => {
-                            const uidString = uids.concat(peer.id).join('-');
-                            const url =`/region/${uidString}/${vector}`;
-                            return _.extend({}, peer, {url});
-                        }).slice(0, 5));
-                    }
-                });
+                Peers.get(uid).then(peers => {
+                    const uids = regions.map(region => region.id);
+                    const vector = params.vector;
+
+                    resolve(peers.filter(peer => {
+                        return uids.indexOf(peer.id) < 0;
+                    }).map(peer => {
+                        const uidString = uids.concat(peer.id).join('-');
+                        const url =`/region/${uidString}/${vector}`;
+                        return _.extend({}, peer, {url});
+                    }).slice(0, 5));
+                }, error => { throw error; });
+
             }
         });
     }
