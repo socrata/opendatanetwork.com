a8ddcc74dddee9ee5a5eaa113eb612f942587590
Lane Aasen <laneaasen@gmail.com>
2016-02-04 20:00:53 -0800

    fixes map variable link formatting

--- src/data/data-sources.js
+++ src/data/data-sources.js
@@ -6,7 +6,30 @@ if (typeof require !== 'undefined') var _ = require('lodash');
  * Data Sources.
  *
  * [Tab] -> [Chart]
+ *
+ * {
+ *
+ *
+ *
  */
+
+const exampleSource = {
+    tabName: 'Example', // Tab names should be kept short.
+    vector: 'example', // Name that goes in the url. Lowercase, underscores, same as map source.
+    name: 'Example Data', // Shown in chart header.
+    attribution: { // See ATTRIBUTIONS.
+        name: 'Example Data Provider',
+        url: 'http://example.com' // Home page.
+    },
+    domain: 'odn.data.socrata.com',
+    fxf: '1234-abcd', // Make sure to use a NBE fxf!
+    regions: ['state', 'county'], // See ALL_REGIONS.
+    charts: [
+        {
+            name: 'Example Chart' // Name passed to chart as 'title' attribute.
+        }
+    ]
+};
 
 const ODN_DOMAIN = 'odn.data.socrata.com';
 
