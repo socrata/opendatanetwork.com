074b272e943433ee423b3ea5a009eeb3dcf7f007
Lane Aasen <laneaasen@gmail.com>
2016-02-19 13:10:31 -0800

    removes synonyms.json

--- controllers/constants.js
+++ controllers/constants.js
@@ -2,7 +2,6 @@
 const Constants = {
     ROSTER_URL: 'https://odn.data.socrata.com/resource/czuf-rtrf.json',
     CATALOG_URL: 'http://api.us.socrata.com/api/catalog/v1',
-    SYNONYMS_FILE: 'data/synonyms.json',
 
     PEERS_URL: 'https://odn-peers.herokuapp.com/peers',
     N_PEERS: 5,
