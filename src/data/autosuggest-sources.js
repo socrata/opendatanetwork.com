c826c6045e908396f2fffeef87770a77f93e270b
Lane Aasen <laneaasen@gmail.com>
2016-03-08 12:12:39 -0800

    updates autocomplete by source

--- src/data/autosuggest-sources.js
+++ src/data/autosuggest-sources.js
@@ -1,27 +1,12 @@
 
-const domain = 'odn.data.socrata.com';
-
-const _nameToColumn = new Map();
-_nameToColumn.set('population', 'population');
-_nameToColumn.set('earnings', 'earnings');
-_nameToColumn.set('education', 'education');
-_nameToColumn.set('occupations', 'occupations');
-_nameToColumn.set('cost_of_living', 'rpp');
-_nameToColumn.set('gdp', 'gdp');
-_nameToColumn.set('health', 'health');
-const defaultColumn = 'all';
-
-const _withDataSource = {
-    name: 'Regions with Data',
-    domain: domain,
-    fxf: '68ht-6puw',
-    encoded: ['id', 'type', 'population'],
-    sort: option => -parseFloat(option.population)
-};
-
-function regionsWithData(name, select) {
-    const column = _nameToColumn.has(name) ? _nameToColumn.get(name) : defaultColumn;
-
-    return [_.extend({}, _withDataSource, {select, column})];
+function regionsWithData(vector, select) {
+    const column = vector;
+    return [_.extend({
+        name: 'Regions with Data',
+        domain: 'odn.data.socrata.com',
+        fxf: '29pm-gr6s',
+        encoded: ['id', 'type', 'population'],
+        sort: option => -parseFloat(option.population)
+    },  {select, column})];
 }
 
