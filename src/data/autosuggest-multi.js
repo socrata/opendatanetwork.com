7e55a03c639bd3b68e343279c76889756b144df4
Lane Aasen <laneaasen@gmail.com>
2016-04-29 19:20:12 -0700

    fixes question url escape issue

--- src/data/autosuggest-multi.js
+++ src/data/autosuggest-multi.js
@@ -1,10 +1,21 @@
 
 // Autocomplete on datasets, regions, publishers, and categories.
 function multiComplete(inputSelector, resultSelector) {
+    function urlEscape(string) {
+        return string
+            .replace(/,/g, '')
+            .replace(/[ \/]/g, '_');
+    }
+
     function navigate(path, params) {
         params = params || {};
         const url = `${path}?${$.param(params)}`;
         window.location.href = url;
+    }
+
+    function path(elements) {
+        return `/${elements.map(urlEscape).join('/')}`;
+
     }
 
     const domain = 'odn.data.socrata.com';
@@ -17,9 +28,7 @@ function multiComplete(inputSelector, resultSelector) {
             fxf: '68ht-6puw',
             column: 'all',
             encoded: ['id', 'type', 'population'],
-            select: option => {
-                navigate(`/region/${option.id}/${option.text.replace(/ /g, '_').replace(/\//g, '_').replace(/,/g, '')}`);
-            },
+            select: option => navigate(path(['region', option.id, option.text])),
             sort: option => -parseFloat(option.population),
             show: (selection, option) => {
                 selection.append('span')
@@ -31,13 +40,33 @@ function multiComplete(inputSelector, resultSelector) {
             }
         },
         {
+            name: 'Questions',
+            image: 'fa-question-circle',
+            domain: domain,
+            fxf: 'd6be-a5xs',
+            column: 'question',
+            encoded: ['regionName', 'regionID', 'regionPopulation',
+                      'source', 'variable', 'metric', 'index'],
+            select: option => navigate(path(
+                ['region', option.regionID, option.regionName, option.source, option.metric])),
+            sort: option => {
+                const population = parseFloat(option.regionPopulation);
+                const index = parseFloat(option.index);
+                return -(population - index);
+            },
+            show: (selection, option) => {
+                selection.append('span')
+                    .text(`What is the ${option.variable} of ${option.regionName}?`);
+            }
+        },
+        {
             name: 'Datasets',
             image: 'fa-bar-chart',
             domain: domain,
             fxf: 'fpum-bjbr',
             column: 'encoded',
             encoded: ['domain', 'fxf'],
-            select: option => navigate(`/dataset/${option.domain}/${option.fxf}`),
+            select: option => navigate(path(['dataset', option.domain, option.fxf])),
             show: (selection, option) => {
                 selection.append('span')
                     .attr('class', 'name')
@@ -66,34 +95,6 @@ function multiComplete(inputSelector, resultSelector) {
                 selection.append('span')
                     .attr('class', 'capitalize')
                     .text(option.text);
-            }
-        },
-        {
-            name: 'Questions',
-            image: 'fa-question-circle',
-            domain: domain,
-            fxf: 'usa3-f8qu',
-            column: 'question',
-            encoded: ['regionName', 'regionID', 'regionPopulation',
-                      'source', 'variable', 'metric', 'index'],
-            select: option => {
-                const regionID = option.regionID;
-                const regionName = option.regionName.replace(/,/g, '').replace(/[ \/]/g, '_');
-                const source = option.source;
-                const metric = option.metric;
-                const url = `/region/${regionID}/${regionName}/${source}/${metric}`;
-
-                navigate(url);
-            },
-            sort: option => {
-                const population = parseFloat(option.regionPopulation);
-                const index = parseFloat(option.index);
-
-                return -(population - index);
-            },
-            show: (selection, option) => {
-                selection.append('span')
-                    .text(`What is the ${option.variable} of ${option.regionName}?`);
             }
         }
     ];
