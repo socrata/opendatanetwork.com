7fbdec4a1b7feed8b8b54aa73dd058bbb09d2d46
Tosh Meston <tosh.meston@gmail.com>
2016-05-04 17:41:00 -0700

    Added single tap handler to expand map when collapsed.

--- src/maps/expand-collapse-control.js
+++ src/maps/expand-collapse-control.js
@@ -3,18 +3,6 @@ const ExpandCollapseControl = L.Control.extend({
 
     options: { position: 'topleft' },
 
-    disableControls: function(map) {
-        map.dragging.disable();
-        map.touchZoom.disable();
-        if (map.tap) map.tap.disable();
-    },
-
-    enableControls: function(map) {
-        map.dragging.enable();
-        map.touchZoom.enable();
-        if (map.tap) map.tap.enable();
-    },
-    
     onAdd: function(map) {
 
         const containerDiv = L.DomUtil.create('div', 'leaflet-bar map-expand-collapse');
@@ -31,35 +19,57 @@ const ExpandCollapseControl = L.Control.extend({
             .attr('class', 'map-expand')
             .on('click', option => {
 
-                if (this.expanded) {
+                if (this.expanded)
+                    this.collapseMap(map);
+                else
+                    this.expandMap(map);
 
-                    $('#leaflet-map').animate({ height: 200 }, null, null, () => {
-                        map.invalidateSize();
-                    });
-
-                    if (!this.buttonIcon.classed('fa-expand'))
-                        this.buttonIcon.classed('fa-expand', true).classed('fa-compress', false);
-
-                    this.disableControls(map);
-               }
-                else {
-
-                    $('#leaflet-map').animate({ height: 500 }, null, null, () => {
-                        map.invalidateSize();
-                    });
-
-                    if (this.buttonIcon.classed('fa-expand'))
-                        this.buttonIcon.classed('fa-expand', false).classed('fa-compress', true);
-
-                    this.enableControls(map);
-                }
-
-                this.expanded = !this.expanded;
+                d3.event.stopPropagation();
             });
 
         this.buttonIcon = this.button.append('i').attr('class', this.expanded ? 'fa fa-compress' : 'fa fa-expand');
 
         return containerDiv;
+    },
+
+    disableControls: function(map) {
+
+        map.dragging.disable();
+        map.touchZoom.disable();
+        if (map.tap) map.tap.disable();
+
+        map.once('click', () => {
+            this.expandMap(map);
+        });
+    },
+
+    enableControls: function(map) {
+
+        map.dragging.enable();
+        map.touchZoom.enable();
+        if (map.tap) map.tap.enable();
+    },
+
+    expandMap: function(map) {
+
+        $('#leaflet-map').animate({ height: 500 }, null, null, () => {
+            map.invalidateSize();
+        });
+        
+        this.buttonIcon.classed('fa-expand', false).classed('fa-compress', true);
+        this.enableControls(map);
+        this.expanded = true;
+    },
+
+    collapseMap: function(map) {
+
+        $('#leaflet-map').animate({ height: 200 }, null, null, () => {
+            map.invalidateSize();    
+        });
+
+        this.buttonIcon.classed('fa-compress', false).classed('fa-expand', true);
+        this.disableControls(map);
+        this.expanded = false;
     }
 });
 
