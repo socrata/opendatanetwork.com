9c1d0fc27bf81c3212dca179ca209a575b01cd34
Sasidharan K <sasidharan@thydreamtech.com>
2016-08-27 18:03:46 +0530

    moved api.js functionality to models

---
+++ app/lib/fileCache.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const _ = require('lodash');
+const moment = require('moment');
+const numeral = require('numeral');
+
+const _fileCache = {};
+
+class FileCache {
+  static get(path) {
+    return new Promise((resolve, reject) => {
+      if (path in _fileCache) {
+        resolve(_fileCache[path]);
+      } else {
+        fs.readFile(`${__dirname}/../${path}`, (error, data) => {
+          if (error) {
+            reject(error);
+          } else {
+            const json = JSON.parse(data);
+            _fileCache[path] = json;
+            resolve(json);
+          }
+        });
+      }
+    });
+  }
+}
+module.exports = FileCache;
---
+++ app/models/place.js
@@ -0,0 +1,24 @@
+const _ = require('lodash');
+const moment = require('moment');
+const numeral = require('numeral');
+const Request = require('../controllers/request');
+const FileCache = require('../lib/fileCache');
+const Constants = require('../controllers/constants');
+
+class Place {
+  static regions(ids) {
+    const params = {'$where': `id in(${ids.map(id => `'${id}'`).join(',')})`};
+    const url = Request.buildURL(Constants.ROSTER_URL, params);
+    return Request.getJSON(url);
+  }
+
+  static locations() {
+    return FileCache.get('data/locations.json');
+  }
+
+  static stateNames() {
+    return FileCache.get('data/state-names.json');
+  }
+}
+
+module.exports = Place;
