eec3fb910e73f38296eae4b9a542f20fc1d1adf2
mubarocks <mubarocks@gmail.com>
2016-08-30 18:02:17 +0530

    1. UI Tweaks to quick links dropdown 2. Added keyboard navigation/tabing support for quick links dropdown 3. Added ADA aria roles 4. Refactored quick links dropdown code

--- src/quick-links.js
+++ src/quick-links.js
@@ -14,8 +14,8 @@ class QuickLinks {
 
         // Mobile
         //
-        $('.quick-links-menu-header-mobile').click(function(event) {
-
+        $('.quick-links-menu-header-mobile').click(function (event) {
+            event.preventDefault();
             event.stopPropagation();
 
             const selectedMenu = '#' + $(this).parent().attr('id');
@@ -26,39 +26,48 @@ class QuickLinks {
             //
             const carets = menusToClose.map(s => (s + ' .quick-links-expand-caret-mobile'));
             $(carets.join(', ')).removeClass('fa-caret-up').addClass('fa-caret-down');
-            
-            const subLists = menusToClose.map(s => (s + '>ul')); 
+
+            const subLists = menusToClose.map(s => (s + '>ul'));
             $(subLists.join(', ')).slideUp();
 
             // Open selected
             //
-            if ($(selectedMenu + '>ul').is(':visible'))
+            if ($(selectedMenu + '>ul').is(':visible')) {
+                $(this).attr('aria-expanded', 'false');
                 $(this).find('.quick-links-expand-caret-mobile').removeClass('fa-caret-up').addClass('fa-caret-down');
-            else
+            } else {
+                $(this).attr('aria-expanded', 'true');
                 $(this).find('.quick-links-expand-caret-mobile').removeClass('fa-caret-down').addClass('fa-caret-up');
+            }
 
-            $(menuToToggle).slideToggle(); 
+            $(menuToToggle).slideToggle();
         });
 
         // Desktop
         //
-        $('.quick-links-more').click(function() {
-
+        $('.quick-links-more').click(function (event) {
+            event.preventDefault();
             $('.quick-links-more').hide();
             $('.quick-links-sub-menu li').removeClass('quick-links-hidden');
         });
 
-        // Common
+        // Mobile
         //
-        $('.quick-links-container').click(function() {
+        $('header .quick-links-container').on('click', '.quick-links-button-narrow', function (event) {
+            event.preventDefault();
+
+            var $this = $(this),
+            $quickLinksContainer = $this.parent('.quick-links-container');
 
             if (self.onShow) self.onShow();
 
-            if ($(this).hasClass('quick-links-container-selected')) {
+            if ($this.hasClass('active')) {
+                $this.removeClass('active');
+                $this.attr('aria-expanded', 'false');
 
-                $(this).removeClass('quick-links-container-selected');
-                $(this).children('.quick-links-menu-container-mobile').hide();
-                
+                $quickLinksContainer.removeClass('quick-links-container-selected');
+                $quickLinksContainer.children('.quick-links-menu-container-mobile').hide();
+
                 // Collapse all sub lists
                 //
                 const carets = mobileSubMenus.map(s => (s + ' .quick-links-expand-caret-mobile'));
@@ -66,34 +75,50 @@ class QuickLinks {
 
                 const subLists = mobileSubMenus.map(s => (s + ' .quick-links-menu-sub-list-mobile'));
                 $(subLists.join(', ')).hide();
-            }
-            else {
-                $(this).addClass('quick-links-container-selected');
-                $(this).children('.quick-links-menu-container-mobile').show();
+            } else {
+                if (self.onShow) self.onShow();
+
+                $this.addClass('active');
+                $this.attr('aria-expanded', 'true');
+
+                $quickLinksContainer.addClass('quick-links-container-selected');
+                $quickLinksContainer.children('.quick-links-menu-container-mobile').show();
             }
         });
 
-        // Do not bind mouse events on mobile
+        // Desktop
         //
-        if (!$('.quick-links-button-narrow').is(':visible')) {
+        $('.home-search-bar-controls').on('click', '.quick-links-button-wide', function (event) {
+            event.preventDefault();
 
-            $('.quick-links-container').mouseover(function() {
+            var $this = $(this),
+                $quickLinksContainer = $this.parent('.quick-links-container');
 
+            if ($this.hasClass('active')) {
+                $this.removeClass('active');
+                $this.attr('aria-expanded', 'false');
+                $quickLinksContainer.removeClass('quick-links-container-selected');
+                $quickLinksContainer.children('.quick-links-button-wide').children('i').removeClass('fa-caret-up').addClass('fa-caret-down');
+                $quickLinksContainer.children('.quick-links-menu-container').hide();
+                $quickLinksContainer.children('.quick-links-menu-container-mobile').hide();
+            } else {
                 if (self.onShow) self.onShow();
 
-                $(this).addClass('quick-links-container-selected');
-                $(this).children('.quick-links-button-wide').children('i').removeClass('fa-caret-down').addClass('fa-caret-up');
-                $(this).children('.quick-links-menu-container').show();
-                $(this).children('.quick-links-menu-container-mobile').show();
-            });
+                $this.addClass('active');
+                $this.attr('aria-expanded', 'true');
+                $quickLinksContainer.addClass('quick-links-container-selected');
+                $quickLinksContainer.children('.quick-links-button-wide').children('i').removeClass('fa-caret-down').addClass('fa-caret-up');
+                $quickLinksContainer.children('.quick-links-menu-container').show();
+                $quickLinksContainer.children('.quick-links-menu-container-mobile').show();
+            }
+        });
 
-            $('.quick-links-container').mouseout(function() {
-
-                $(this).removeClass('quick-links-container-selected');
-                $(this).children('.quick-links-button-wide').children('i').removeClass('fa-caret-up').addClass('fa-caret-down');
-                $(this).children('.quick-links-menu-container').hide();
-                $(this).children('.quick-links-menu-container-mobile').hide();
-            });
-        }
+        // Hiding quick links pop when click outside
+        //
+        $(document).on('click touch', function (event) {
+          if (!$(event.target).parents().addBack().is('.quick-links-container')) {
+            $('.home-search-bar-controls .quick-links-button-wide.active').click();
+          }
+        });
     }
 }
