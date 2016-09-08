bdb1a297e0c44e8a604f83360b4702f5094169e9
Lane Aasen <laneaasen@gmail.com>
2016-09-07 17:38:40 -0700

    removes more quicklink files

--- gulpfile.js
+++ gulpfile.js
@@ -59,7 +59,6 @@ function js(src, dest) {
 var homeScripts = baseScripts
     .concat(autosuggestScripts)
     .concat([
-        'src/quick-links.js',
         'src/home-page-controller.js',
         'src/home.js']);
 gulp.task('home', js(homeScripts, 'home.min.js'));
@@ -67,7 +66,6 @@ gulp.task('home', js(homeScripts, 'home.min.js'));
 var searchScripts = baseScripts
     .concat(autosuggestScripts)
     .concat([
-        'src/quick-links.js',
         'src/search-refine-controls-mobile.js',
         'src/infinite-scroll.js',
         'src/search.js',
@@ -87,7 +85,6 @@ gulp.task('entity', js(entityScripts, 'entity.min.js'));
 var datasetScripts = baseScripts
     .concat(autosuggestScripts)
     .concat([
-        'src/quick-links.js',
         'src/dataset.js']);
 gulp.task('dataset', js(datasetScripts, 'dataset.min.js'));
 
