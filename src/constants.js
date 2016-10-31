eeb5a735ff6d92d1267bf25c5775c18bbc4ee1d2
Alicia Brown <alicia.brown@aliciabrown.local>
2016-10-31 14:52:41 -0700

    Added local api links commented out

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // prod
- const _ODN_API_HOST_AND_PORT = 'api.opendatanetwork.com';
- const _ODN_API_BASE_URL = 'https://'+_ODN_API_HOST_AND_PORT;
+const _ODN_API_HOST_AND_PORT = 'api.opendatanetwork.com';
+const _ODN_API_BASE_URL = 'https://'+_ODN_API_HOST_AND_PORT;
 
 // local
 // const _ODN_API_HOST_AND_PORT = 'localhost:3001';
