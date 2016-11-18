c9ac655bf53d89f0afccbca56c91e1fe877f9763
Chris Metcalf <chris@chrismetcalf.net>
2016-11-18 14:38:12 -0800

    You can now pass an CONFIG_URL
    Allows you to override config in different environments

--- src/config.js
+++ src/config.js
@@ -2,10 +2,26 @@
 
 const yaml = require('js-yaml');
 const fs = require('fs');
+const request = require('sync-request');
+const deepmerge = require('deepmerge');
 
-// TODO: Cache config
-// TODO: Allow config overrides
-const GlobalConfig = yaml.safeLoad(fs.readFileSync(__dirname + '/../config.yml', 'utf8'));
+// Yes, I know this is all synchronous, but it also only happens at startup time
+var loadConfig = function() {
+  var config = yaml.safeLoad(fs.readFileSync(__dirname + '/../config.yml', 'utf8'));
+
+  // Fetch our override config if we've got it
+  if(process.env.CONFIG_URL) {
+    var res = request('GET', process.env.CONFIG_URL);
+    if(res.statusCode === 200) {
+      var override = JSON.parse(res.getBody('utf8'));
+      config = deepmerge.all([config, override]);
+    }
+  }
+
+  return config;
+};
+
+const GlobalConfig = loadConfig();
 
 /**
  * Makes this accessible inside of server side executed controllers stuff
